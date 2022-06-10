d12bb0d201fe9e8b844ea432fffb1ed5106af692
deez nutz <thenozzle@the-nozzle.bushleague.net>
2022-06-09 21:34:59 -0700

    rustlings solutions

---
+++ rust_things/rustlings/exercises/clippy/target/debug/deps/clippy2-8710b3b2f3dd0277.d
@@ -0,0 +1,5 @@
+/Users/thenozzle/src/scratch_work/rusty/rustlings/exercises/clippy/target/debug/deps/clippy2-8710b3b2f3dd0277.rmeta: clippy2.rs
+
+/Users/thenozzle/src/scratch_work/rusty/rustlings/exercises/clippy/target/debug/deps/clippy2-8710b3b2f3dd0277.d: clippy2.rs
+
+clippy2.rs:
---
+++ rust_things/rustlings/exercises/clippy/target/debug/deps/clippy2-a967fe219cd67e11.d
@@ -0,0 +1,7 @@
+/Users/thenozzle/src/scratch_work/rusty/rustlings/exercises/clippy/target/debug/deps/clippy2-a967fe219cd67e11.rmeta: clippy2.rs
+
+/Users/thenozzle/src/scratch_work/rusty/rustlings/exercises/clippy/target/debug/deps/clippy2-a967fe219cd67e11.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
