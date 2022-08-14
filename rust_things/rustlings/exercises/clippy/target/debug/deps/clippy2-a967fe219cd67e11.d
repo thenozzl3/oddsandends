e5eb130405285e91c6880d6ba589205668e08c82
deez nutz <thenozzle@the-nozzle.local>
2022-08-14 10:42:35 -0700

    stuff

--- rust_things/rustlings/exercises/clippy/target/debug/deps/clippy2-a967fe219cd67e11.d
+++ rust_things/rustlings/exercises/clippy/target/debug/deps/clippy2-a967fe219cd67e11.d
@@ -1,6 +1,6 @@
-/Users/thenozzle/src/scratch_work/rusty/rustlings/exercises/clippy/target/debug/deps/clippy2-a967fe219cd67e11.rmeta: clippy2.rs
+/Users/thenozzle/src/oddsandends/rust_things/rustlings/exercises/clippy/target/debug/deps/clippy2-a967fe219cd67e11.rmeta: clippy2.rs
 
-/Users/thenozzle/src/scratch_work/rusty/rustlings/exercises/clippy/target/debug/deps/clippy2-a967fe219cd67e11.d: clippy2.rs
+/Users/thenozzle/src/oddsandends/rust_things/rustlings/exercises/clippy/target/debug/deps/clippy2-a967fe219cd67e11.d: clippy2.rs
 
 clippy2.rs:
 
